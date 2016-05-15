7337ee4de38dfbebd6ed4646dac1fe8bcda7bec5
Zhuk Pavel <ppzhuk@users.noreply.github.com>
2016-05-15 16:35:49 +0300

    add gitignore and minor script changes

--- dataFetcher/dataFetcher.php
+++ dataFetcher/dataFetcher.php
@@ -118,4 +118,6 @@
        
         closeConnections();
     }
+    
+    fetchData();
 ?>
--- http/config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=info_table',
-    'username' => 'root',
-    'password' => 'sky211993',
-    'charset' => 'utf8',
-];
